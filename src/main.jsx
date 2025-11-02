8394288c4bdcbc21eb7fd11e5b88221046a36325
MarekP <mwpolomw@wp.pl>
2025-11-02 17:44:29 +0100

    zmiana nazwwy pliku Main.scss

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { MainIntro } from './MainIntro'
 import { MainIntroFooter } from './MainIntroFooter'
 import { MainContent } from './MainContent'
 
-import './Main.scss'
+import './Main-style.scss'
 
 export function Main () {
   return (
